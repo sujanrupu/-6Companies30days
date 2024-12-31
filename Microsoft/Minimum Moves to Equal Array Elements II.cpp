ef8a69683717a6f54cf03cc0b191e772df98435d
Sujan Ghosh <103595490+sujanrupu@users.noreply.github.com>
2024-12-31 14:02:09 +0530

    Create Minimum Moves to Equal Array Elements II.cpp

---
+++ Microsoft/Minimum Moves to Equal Array Elements II.cpp
@@ -0,0 +1,14 @@
+// Link: https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii/submissions/1493165014/
+
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int median = nums[nums.size() / 2];
+        int moves = 0;
+        for (int num : nums) {
+            moves += abs(num - median);
+        }
+        return moves;
+    }
+};
